053c228c27f019eea97e1be21583939a77031729
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-11 07:58:38 -0400

    increase padding x on larger screens

--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -4,7 +4,7 @@ import EventName from './eventName'
 
 export default function matchRow({ match }) {
     return (
-        <li className='flex justify-between items-center my-4 p-6 rounded bg-gray-900'>
+        <li className='flex justify-between items-center my-4 p-6 md:px-16 rounded bg-gray-900'>
             <MatchTime matchTime={match.match_time} matchEta={match.eta}/>
             <MatchTeams teamOneName={match.team_one_name} teamTwoName={match.team_two_name} />
             <EventName eventName={match.event_name} eventImg={match.event_icon_url} />
