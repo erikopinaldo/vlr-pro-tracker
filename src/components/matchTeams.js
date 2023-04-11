efb504dd0513f39937d4e011279f4cae19d7c4e9
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-10 22:22:22 -0400

    decrease font size

--- src/components/matchTeams.js
+++ src/components/matchTeams.js
@@ -1,6 +1,6 @@
 export default function MatchTeams({ teamOneName, teamTwoName }) {
     return (
-        <div className="basis-1/2 mx-4">
+        <div className="basis-1/2 mx-4 text-sm">
             <span>{teamOneName} vs. {teamTwoName}</span>
         </div>
     );
