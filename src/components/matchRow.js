cb5ad882e592236994a31010b9fdfd8984e6b1ed
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-11 07:23:21 -0400

    pass matchEta prop

--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -5,7 +5,7 @@ import EventName from './eventName'
 export default function matchRow({ match }) {
     return (
         <li className='flex justify-between items-center my-4 p-6 rounded bg-gray-900'>
-            <MatchTime matchTime={match.match_time} />
+            <MatchTime matchTime={match.match_time} matchEta={match.eta}/>
             <MatchTeams teamOneName={match.team_one_name} teamTwoName={match.team_two_name} />
             <EventName eventName={match.event_icon_url} />
         </li>
