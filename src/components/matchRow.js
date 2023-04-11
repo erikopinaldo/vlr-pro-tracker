dc0b52f77c04c8e1af45a2a2b287852559b0ee25
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-10 22:09:37 -0400

    decrease padding

--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -4,7 +4,7 @@ import EventName from './eventName'
 
 export default function matchRow({ match }) {
     return (
-        <li className='flex justify-between my-4 p-8 rounded bg-gray-900'>
+        <li className='flex justify-between my-4 p-6 rounded bg-gray-900'>
             <MatchTime matchTime={match.match_time} />
             <MatchTeams teamOneName={match.team_one_name} teamTwoName={match.team_two_name} />
             <EventName eventName={match.event_name} />
