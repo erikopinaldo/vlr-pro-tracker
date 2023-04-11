a45c8dc858fcac9b9e5ec80e184249e56a41f25d
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-11 08:01:24 -0400

    put each team in a separate div

--- src/components/matchTeams.js
+++ src/components/matchTeams.js
@@ -1,7 +1,12 @@
 export default function MatchTeams({ teamOneName, teamTwoName }) {
     return (
         <div className="basis-1/2 mx-4 text-sm">
-            <span>{teamOneName} vs. {teamTwoName}</span>
+            <div>
+                <span>{teamOneName}</span>
+            </div>
+            <div>
+                <span>{teamTwoName}</span>
+            </div>
         </div>
     );
 }
