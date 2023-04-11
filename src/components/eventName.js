278deada79516425d96c955c377c9a1a22491919
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-11 07:56:06 -0400

    set justify to flex-end for parent div

--- src/components/eventName.js
+++ src/components/eventName.js
@@ -1,8 +1,7 @@
 export default function EventName({ eventName, eventImg }) {
     return (
-        <div className="flex items-center basis-1/4 text-right">
+        <div className="flex items-center basis-1/4 justify-end">
             <span className="hidden md:inline mr-12 text-sm">{eventName}</span>
-
             <img src={eventImg} />
         </div>
     );
