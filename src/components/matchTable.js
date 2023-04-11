08dbdf067beebd3255373573bb0dec8084ceb4fb
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-10 22:03:28 -0400

    change background colour

--- src/components/matchTable.js
+++ src/components/matchTable.js
@@ -12,7 +12,7 @@ export default function MatchTable({ matches }) {
     });
     
     return (
-        <div className='mx-2 my-20 p-8 rounded bg-slate-900'>
+        <div className='mx-2 my-20 p-8 rounded bg-gray-950'>
             <ul>{rows}</ul>
         </div>
     );
