e305451669dd64562b56f4aa6cbe6ff528c7dff9
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-10 21:53:02 -0400

    add background colour

--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -4,7 +4,7 @@ import EventName from './eventName'
 
 export default function matchRow({ match }) {
     return (
-        <li className='flex justify-between my-4 px-8'>
+        <li className='flex justify-between my-4 px-8 bg-slate-500'>
             <MatchTime matchTime={match.match_time} />
             <MatchTeams teamOneName={match.team_one_name} teamTwoName={match.team_two_name} />
             <EventName eventName={match.event_name} />
