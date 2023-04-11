bad73472725dc1a5cf57a8a3b372e02d7838a448
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-11 07:42:35 -0400

    remove justify-between on parent div

--- src/components/eventName.js
+++ src/components/eventName.js
@@ -1,6 +1,6 @@
 export default function EventName({ eventName, eventImg }) {
     return (
-        <div className="basis-1/4 text-right">
+        <div className="flex items-center basis-1/4 text-right">
             <span className="hidden md:inline">{eventName}</span>
 
             <img src={eventImg} />
