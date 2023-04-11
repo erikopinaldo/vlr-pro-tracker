f06022b57b4369a900dc3104431264304caac9d8
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-10 21:49:32 -0400

    add margin x to div

--- src/components/matchTeams.js
+++ src/components/matchTeams.js
@@ -1,6 +1,6 @@
 export default function MatchTeams({ teamOneName, teamTwoName }) {
     return (
-        <div className="basis-1/2">
+        <div className="basis-1/2 mx-4">
             <span>{teamOneName} vs. {teamTwoName}</span>
         </div>
     );
