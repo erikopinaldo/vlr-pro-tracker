b3474a4cee3671ac3ca07883d36bc5b44311863f
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-11 07:46:19 -0400

    add margin right

--- src/components/eventName.js
+++ src/components/eventName.js
@@ -1,7 +1,7 @@
 export default function EventName({ eventName, eventImg }) {
     return (
         <div className="flex items-center basis-1/4 text-right">
-            <span className="hidden md:inline">{eventName}</span>
+            <span className="hidden md:inline mr-12 text-sm">{eventName}</span>
 
             <img src={eventImg} />
         </div>
