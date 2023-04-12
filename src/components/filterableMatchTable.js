43fbeb19a196bb0f4b97b780dc60e1dba67dbbf3
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 13:16:55 -0400

    pass filterArr prop to FilterBar and MatchTable components

--- src/components/filterableMatchTable.js
+++ src/components/filterableMatchTable.js
@@ -1,11 +1,18 @@
 import FilterBar from './filterBar'
 import MatchTable from './matchTable'
+import { useState } from 'react';
 
 export default function FilterableMatchTable({ matches }) {
+    const [filterArr, setFilterArr] = useState([]);
+    
     return (
         <div>
-            <FilterBar matches={matches} />
-            <MatchTable matches={matches} />
+            <FilterBar
+                matches={matches}
+                filterArr={filterArr} />
+            <MatchTable
+                matches={matches}
+                filterArr={filterArr} />
         </div>
     );
 }
