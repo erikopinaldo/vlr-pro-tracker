b8cafad7f5eb7d27580414822b20f0dc0f072625
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 15:44:04 -0400

    use onFilterClick handler on li

--- src/components/filterItem.js
+++ src/components/filterItem.js
@@ -1,6 +1,8 @@
-export default function filterItem({ eventName }) {
+export default function filterItem({ eventName, onFilterClick }) {
     return (
-        <li className='my-4 pl-8 py-2 md:px-16 rounded-full bg-gray-900'>
+        <li
+            className='my-4 pl-8 py-2 md:px-16 rounded-full bg-gray-900'
+            onClick={onFilterClick()}>
             <span>{eventName}</span>
         </li>
     );
