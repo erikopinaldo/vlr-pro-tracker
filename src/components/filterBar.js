ef86bad99eca3b2dbd22d695dfd27cbbe70c5eec
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 12:50:08 -0400

    initial FilterBar component

---
+++ src/components/filterBar.js
@@ -0,0 +1,17 @@
+import FilterItem from './filterItem'
+
+export default function FilterBar({ matches }) {
+    const rows = []
+
+    matches.forEach(match => {
+        rows.push(
+            <FilterItem eventName={match.event_name} />
+        );
+    });
+
+    return (
+        <div className='my-6 px-4 py-2 bg-gray-950'>
+            <ul>{rows}</ul>
+        </div>
+    );
+}
