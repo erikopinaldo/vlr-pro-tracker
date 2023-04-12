f4ddcc3844c04f1016fb6d7663723cf615466b54
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 13:17:46 -0400

    dont build MatchRow for match that doesnt match filter

--- src/components/matchTable.js
+++ src/components/matchTable.js
@@ -1,9 +1,13 @@
 import MatchRow from './matchRow'
 
-export default function MatchTable({ matches }) {
+export default function MatchTable({ matches, filterArr }) {
     const rows = []
 
     matches.forEach(match => {
+        if (filterArr.length > 0 && filterArr.indexOf(match.event_name) === -1) {
+            return;
+        }
+        
         rows.push(
             <MatchRow
                 match={match}
