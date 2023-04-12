a8a7f9d93c37124b76ac41f9b0e10c71756093cf
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 16:14:04 -0400

    add cursor-pointer tailwind class

--- src/components/filterItem.js
+++ src/components/filterItem.js
@@ -1,7 +1,7 @@
 export default function filterItem({ eventName, onFilterClick }) {
     return (
         <li
-            className='my-4 pl-8 py-2 md:px-16 rounded-full bg-gray-900'
+            className='my-4 pl-8 py-2 md:px-16 rounded-full bg-gray-900 cursor-pointer'
             onClick={onFilterClick()}>
             <span>{eventName}</span>
         </li>
