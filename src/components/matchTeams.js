ac4a5be0f87bf9cda524503fc9914cf81de82855
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 19:30:51 -0400

    remove Tailwind font size class

--- src/components/matchTeams.js
+++ src/components/matchTeams.js
@@ -1,6 +1,6 @@
 export default function MatchTeams({ teamOneName, teamTwoName }) {
     return (
-        <div className="basis-1/2 mx-4 text-sm">
+        <div className="basis-1/2 mx-4">
             <div>
                 <span>{teamOneName}</span>
             </div>
