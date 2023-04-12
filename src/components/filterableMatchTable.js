4b59ec4cdb0195df16ee35b4826c6f311eea3401
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 15:43:09 -0400

    create handleFilterClick handler and pass to FilterBar component

--- src/components/filterableMatchTable.js
+++ src/components/filterableMatchTable.js
@@ -4,12 +4,19 @@ import { useState } from 'react';
 
 export default function FilterableMatchTable({ matches }) {
     const [filterArr, setFilterArr] = useState([]);
+
+    function handleFilterClick(eventName) {
+        console.log(eventName)
+        const nextFilter = [...filterArr, eventName]
+        setFilterArr(nextFilter)
+    }
     
     return (
         <div>
             <FilterBar
                 matches={matches}
-                filterArr={filterArr} />
+                filterArr={filterArr}
+                onFilterClick={(e) => handleFilterClick(e.target.textContent)} />
             <MatchTable
                 matches={matches}
                 filterArr={filterArr} />
