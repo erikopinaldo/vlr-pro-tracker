a3de1b7ef9ab8b36350b5edc63dd73fca6729926
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 23:03:38 -0400

    div now takes up 2 columns instead of 3

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -29,7 +29,7 @@ export default function FilterBar({ matches, filterArr, onFilterClick, onFilterR
     ))
 
     return (
-        <div className='my-6 md:my-0 px-4 py-2 bg-gray-950 md:col-span-3'>
+        <div className='my-6 md:my-0 px-4 py-2 bg-gray-950 md:col-span-2'>
             <ul>{rows}</ul>
         </div>
     );
