406858436f933031af8095171a80664ec9f0b104
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 21:13:59 -0400

    remove margin y on larger screens

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -6,7 +6,7 @@ export default function FilterBar({ matches, filterArr, onFilterClick, onFilterR
 
     matches.forEach(match => {
         let isActive = filterArr.includes(match.event_name)
-        
+
         if (!events.includes(match.event_name)) {
             events.push(match.event_name)
 
@@ -29,7 +29,7 @@ export default function FilterBar({ matches, filterArr, onFilterClick, onFilterR
     ))
 
     return (
-        <div className='my-6 px-4 py-2 bg-gray-950 md:col-span-3'>
+        <div className='my-6 md:my-0 px-4 py-2 bg-gray-950 md:col-span-3'>
             <ul>{rows}</ul>
         </div>
     );
--- src/components/matchTable.js
+++ src/components/matchTable.js
@@ -16,7 +16,7 @@ export default function MatchTable({ matches, filterArr }) {
     });
     
     return (
-        <div className='my-6 px-4 py-2 md:col-span-7'>
+        <div className='my-6 md:my-0 px-4 py-2 md:col-span-7'>
             <ul>{rows}</ul>
         </div>
     );
