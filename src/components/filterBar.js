3586d3c595a4cd7a1c6458ea52c40817e9f53e6b
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 20:04:35 -0400

    add col-span-3 class on larger screens

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -18,7 +18,7 @@ export default function FilterBar({ matches, onFilterClick }) {
     });
 
     return (
-        <div className='my-6 px-4 py-2 bg-gray-950'>
+        <div className='my-6 px-4 py-2 bg-gray-950 md:col-span-3'>
             <ul>{rows}</ul>
         </div>
     );
