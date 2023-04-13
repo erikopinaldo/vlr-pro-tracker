5748956729c8e66d6c558f9ab6e72f32cfa364c4
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 20:22:24 -0400

    add reset filter li

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -17,6 +17,12 @@ export default function FilterBar({ matches, onFilterClick }) {
         }
     });
 
+    rows.push((
+        <li key='reset' className='text-center'>
+            Reset filter
+        </li>
+    ))
+
     return (
         <div className='my-6 px-4 py-2 bg-gray-950 md:col-span-3'>
             <ul>{rows}</ul>
