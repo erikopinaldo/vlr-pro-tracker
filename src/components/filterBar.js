03033ea5ef4af9861ef1256edf5c66bf3c9fee24
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-13 08:33:06 -0400

    move reset filter button to top of filter list

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -4,6 +4,14 @@ export default function FilterBar({ matches, filterArr, onFilterClick, onFilterR
     const rows = []
     const events = []
 
+    rows.push((
+        <li key='reset'
+            className='my-4 pl-8 py-2 md:px-6 text-center cursor-pointer'
+            onClick={onFilterReset()}>
+            Reset filter
+        </li>
+    ))
+
     matches.forEach(match => {
         const isActive = filterArr.includes(match.event_name)
 
@@ -19,14 +27,6 @@ export default function FilterBar({ matches, filterArr, onFilterClick, onFilterR
             );
         }
     });
-
-    rows.push((
-        <li key='reset'
-            className='text-center cursor-pointer'
-            onClick={onFilterReset()}>
-            Reset filter
-        </li>
-    ))
 
     return (
         <div className='my-6 md:my-0 px-4 py-2 bg-gray-950 md:col-span-2'>
