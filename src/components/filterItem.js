87b5235241b19b519d83cf8fb45f35f61d125917
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-12 23:03:56 -0400

    decrease padding x

--- src/components/filterItem.js
+++ src/components/filterItem.js
@@ -1,5 +1,5 @@
 export default function filterItem({ eventName, isActive, onFilterClick }) {
-    let options = 'my-4 pl-8 py-2 md:px-16 rounded-full bg-gray-900 cursor-pointer'
+    let options = 'my-4 pl-8 py-2 md:px-6 rounded-full bg-gray-900 cursor-pointer'
 
     if (isActive) {
         options += ' outline outline-offset-2 outline-color-white'
