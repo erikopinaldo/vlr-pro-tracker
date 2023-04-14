2c96cf89aabc701d881db7578d7d18e71ab7cb19
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-14 10:48:33 -0400

    fix bg colour

--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -4,7 +4,7 @@ import EventName from './eventName'
 
 export default function matchRow({ match, matchTime }) {
     return (
-        <li className='flex justify-between items-center my-4 p-6 md:px-16 rounded bg-gray-800'>
+        <li className='flex justify-between items-center my-4 p-6 md:px-16 rounded bg-cyan-950'>
             <MatchTime matchTime={matchTime} matchEta={match.time_until_match} />
             <MatchTeams teamOneName={match.team1} teamTwoName={match.team2} />
             <EventName eventName={match.tournament_name} eventImg={match.tournament_icon} />
