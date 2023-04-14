28f7387d88dfed55d72558760e3e7db2059ed70c
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-14 10:55:17 -0400

    remove div margin top on smaller screens

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -29,7 +29,7 @@ export default function FilterBar({ matches, filterArr, onFilterClick, onFilterR
     });
 
     return (
-        <div className='my-6 md:my-0 px-4 py-2 bg-gray-950 md:col-span-2'>
+        <div className='m-0 md:mt-4 px-4 py-2 bg-gray-950 md:col-span-2'>
             <ul>{rows}</ul>
         </div>
     );
