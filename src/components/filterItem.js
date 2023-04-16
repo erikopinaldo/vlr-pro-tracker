def6d19c4311feb3e7bdb9ca73d95bbbf507a0ec
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-15 20:11:11 -0400

    use opactiy to indicate active and inactive filters

--- src/components/filterItem.js
+++ src/components/filterItem.js
@@ -1,8 +1,9 @@
 export default function filterItem({ eventName, isActive, onFilterClick }) {
     let options = 'my-4 p-8 py-2 md:px-6 rounded-full bg-gray-800 cursor-pointer truncate'
 
-    if (isActive) {
-        options += ' outline outline-offset-2 outline-color-white'
+    if (!isActive) {
+        // options += ' outline outline-offset-2 outline-color-white'
+        options += ' opacity-50'
     }
     
     return (
