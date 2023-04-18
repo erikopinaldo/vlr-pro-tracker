3638a1e4c77537c4d19189901ce9c594014599c4
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-18 09:52:23 -0400

    use new domain for meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,15 +9,15 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="View upcoming pro Valorant matches" key='description' />
 
         {/* Facebook Meta Tags */}
-        <meta property="og:url" content="https://vlr-pro-tracker.vercel.app/" key='og-url' />
+        <meta property="og:url" content="https://www.post-plant.com/" key='og-url' />
         <meta property="og:type" content="website" key='og-type' />
         <meta property="og:title" content="Pro Valorant Match Schedules" key='og-title' />
         <meta property="og:description" content="View upcoming pro Valorant matches" key='og-description' />
 
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" key='twitter-card' />
-        <meta property="twitter:domain" content="vlr-pro-tracker.vercel.app" key='twitter-domain' />
-        <meta property="twitter:url" content="https://vlr-pro-tracker.vercel.app/" key='twitter-url' />
+        <meta property="twitter:domain" content="www.post-plant.com/" key='twitter-domain' />
+        <meta property="twitter:url" content="https://www.post-plant.com/" key='twitter-url' />
         <meta name="twitter:title" content="Pro Valorant Match Schedules" key='twitter-title' />
         <meta name="twitter:description" content="View upcoming pro Valorant matches" key='twitter-description' />
       </Head>
