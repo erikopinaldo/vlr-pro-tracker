39cd68bd377e9624c834c2baf7325509bfe8b5de
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-17 23:01:56 -0400

    do not render mobile modal content during loading state

--- src/components/filterableMatchTable.js
+++ src/components/filterableMatchTable.js
@@ -53,54 +53,53 @@ export default function FilterableMatchTable() {
     
     return (
         <section className='block md:w-11/12 md:grid md:grid-cols-10'>
-            <div className='block md:hidden my-4 flex justify-center'>
-                <button
-                    className='inline-block md:hidden mx-4'
-                    onClick={openModal}>
-                    Filters
-                </button>
-            </div>
-            <div className='block md:hidden flex items-center'>
-                <Modal
-                    className='m-10 bg-gray-950 rounded'
-                    isOpen={modalIsOpen}
-                    onRequestClose={closeModal}
-                    contentLabel="Filter modal"
-                >
-                    <div className='flex justify-center'>
-                        <button
-                            className='m-4 px-6 py-2 bg-red-700 rounded'
-                            onClick={closeModal}>
-                            close
-                        </button>
-                    </div>
-
-                    <FilterBar
-                        styleOptions='filterModalContent pb-4 px-4 bg-gray-950 overflow-scroll'
-                        matches={matches}
-                        filterArr={filterArr}
-                        onFilterClick={(e) => handleFilterClick(e.target.textContent)}
-                        onFilterReset={() => handleFilterReset} />
-                </Modal>
-            </div>
-
             {matches.length === 0 ? (
                 // Loading skeleton
                 <>
-                    <div className="hidden md:block m-0 md:mt-4 px-4 py-2 bg-gray-950 md:col-span-2 rounded-full animate-pulse">
-                        <div className='h-8 mt-8 mb-4 p-10 py-2 md:px-6 rounded-full bg-gray-800 rounded-full'></div>
-                        <div className='h-8 my-4 p-8 py-2 md:px-6 rounded-full bg-gray-800 rounded-full'></div>
-                        <div className='h-8 my-4 p-8 py-2 md:px-6 rounded-full bg-gray-800 rounded-full'></div>
+                    <div className="hidden md:block mt-8 px-4 py-2 bg-gray-950 md:col-span-2 rounded-full animate-pulse">
+                        <div className='h-8 my-4 rounded-full bg-gray-800 rounded-full'></div>
+                        <div className='h-8 my-4 rounded-full bg-gray-800 rounded-full'></div>
+                        <div className='h-8 my-4 rounded-full bg-gray-800 rounded-full'></div>
                     </div>
-                    <div className='px-4 py-0 md:py-2 md:col-span-7 animate-pulse'>
-                        <div className='h-16 flex justify-between items-center mt-12 mb-4 p-6 md:px-16 rounded bg-sky-950'></div>
-                        <div className='h-16 flex justify-between items-center my-4 p-6 md:px-16 rounded bg-sky-950'></div>
-                        <div className='h-16 flex justify-between items-center my-4 p-6 md:px-16 rounded bg-sky-950'></div>
+                    <div className='mt-24 md:mt-12 px-4 py-0 md:py-2 md:col-span-7 animate-pulse'>
+                        <div className='h-24 md:h-16 mb-4 rounded bg-sky-950'></div>
+                        <div className='h-24 md:h-16 my-4 rounded bg-sky-950'></div>
+                        <div className='h-24 md:h-16 my-4 rounded bg-sky-950'></div>
                     </div>
                 </>
             ) : (
                     // Actual content
                     <>
+                        <div className='block md:hidden my-4 flex justify-center'>
+                            <button
+                                className='inline-block md:hidden mx-4'
+                                onClick={openModal}>
+                                Filters
+                            </button>
+                        </div>
+                        <div className='block md:hidden flex items-center'>
+                            <Modal
+                                className='m-10 bg-gray-950 rounded'
+                                isOpen={modalIsOpen}
+                                onRequestClose={closeModal}
+                                contentLabel="Filter modal"
+                            >
+                                <div className='flex justify-center'>
+                                    <button
+                                        className='m-4 px-6 py-2 bg-red-700 rounded'
+                                        onClick={closeModal}>
+                                        close
+                                    </button>
+                                </div>
+
+                                <FilterBar
+                                    styleOptions='filterModalContent pb-4 px-4 bg-gray-950 overflow-scroll'
+                                    matches={matches}
+                                    filterArr={filterArr}
+                                    onFilterClick={(e) => handleFilterClick(e.target.textContent)}
+                                    onFilterReset={() => handleFilterReset} />
+                            </Modal>
+                        </div>
                         <FilterBar
                             styleOptions='hidden md:block m-0 md:mt-4 px-4 py-2 bg-gray-950 md:col-span-2 overflow-hidden'
                             matches={matches}
