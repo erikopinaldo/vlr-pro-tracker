f8a9c207719075049493c22df1f9edfd0c8c5c7d
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-20 12:25:48 -0400

    hide match ETA on mobile if its not LIVE or Upcoming

--- src/components/matchTime.js
+++ src/components/matchTime.js
@@ -1,10 +1,13 @@
 export default function MatchTime({ matchTime, matchEta }) {
+    let styleOptions 
+    if (matchEta !== 'LIVE' && matchEta !== 'Upcoming') styleOptions = 'hidden md:block'
+
     return (
-        <div className='basis-1/5'>
+        <div className='basis-1/6 mr-10'>
             <div>
                 <span>{matchTime}</span>
             </div>
-            <div>
+            <div className={styleOptions}>
                 <span>{matchEta}</span>
             </div>
         </div>
