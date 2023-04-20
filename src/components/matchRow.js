b959c7b71a3621edbd75eb952e3deb57f587e6f9
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-20 12:27:15 -0400

    use match score component

--- src/components/matchRow.js
+++ src/components/matchRow.js
@@ -1,5 +1,6 @@
 import MatchTime from './matchTime'
 import MatchTeams from './matchTeams'
+import MatchScore from './matchScore'
 import EventName from './eventName'
 
 export default function matchRow({ match, matchTime, matchTimeRelative }) {
@@ -7,6 +8,7 @@ export default function matchRow({ match, matchTime, matchTimeRelative }) {
         <li className='flex justify-between items-center my-4 p-6 md:px-16 rounded bg-sky-950'>
             <MatchTime matchTime={matchTime} matchEta={matchTimeRelative} />
             <MatchTeams teamOneName={match.team1} teamTwoName={match.team2} />
+            <MatchScore score1={match.score1} score2={match.score2} />
             <EventName eventName={match.tournament_name} eventImg={match.tournament_icon} />
         </li>
     );
