f07e9b6a142237f4abebe4db2c324ff7d4e2c801
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-20 12:45:12 -0400

    decrease team div on desktop

--- src/components/matchTeams.js
+++ src/components/matchTeams.js
@@ -1,6 +1,6 @@
 export default function MatchTeams({ teamOneName, teamTwoName }) {
     return (
-        <div className="basis-3/6">
+        <div className="basis-3/6 md:basis-2/6">
             <div>
                 <span>{teamOneName}</span>
             </div>
