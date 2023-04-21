fd6dba1e5d26c8b46245379de2e5960e99c6eef6
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-21 12:48:15 -0400

    import react-tooltip stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import MatchTable from '../components/matchTable'
 import FilterBar from '../components/filterBar'
 import FilterableMatchTable from '../components/filterableMatchTable'
 import NavBar from '../components/navbar'
+import 'react-tooltip/dist/react-tooltip.css'
 import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
