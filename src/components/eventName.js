ad9b29bba6692efa00aa5f8fac03ad01d83043fe
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-21 13:55:13 -0400

    use tooltip anchor tag

--- src/components/eventName.js
+++ src/components/eventName.js
@@ -1,7 +1,16 @@
+import { Tooltip } from 'react-tooltip'
+
 export default function EventName({ eventName, eventImg }) {
     return (
         <div className="flex items-center basis-1/6 md:basis-2/5 justify-end overflow-hidden">
-            <span className="hidden md:inline mr-12 truncate">{eventName}</span>
+            <a
+                className="hidden md:inline mr-12 truncate"
+                data-tooltip-id="event-name-tooltip"
+                data-tooltip-content={eventName}
+                data-tooltip-place="top"
+            >
+                {eventName}
+            </a>
             <img src={eventImg} />
         </div>
     );
