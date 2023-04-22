949e7f6d9fe83633014905de1824477d78585dfa
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-22 13:30:06 -0400

    suppress React hydration warning for mathTime

--- src/components/matchTime.js
+++ src/components/matchTime.js
@@ -3,9 +3,13 @@ export default function MatchTime({ matchTime, matchEta }) {
     if (matchEta !== 'LIVE' && matchEta !== 'Upcoming') styleOptions = 'hidden md:block'
 
     return (
-        <div className='basis-1/6 mr-10'>
+        // We are going to suppress the React hydration warning because matchTime is a relative time
+        // There's always going to be some mismatch between the time calulated on the server and the time calculated on the client
+        // https://react.dev/reference/react-dom/client/hydrateRoot#suppressing-unavoidable-hydration-mismatch-errors
+        <div
+            className='basis-1/6 mr-10'>
             <div>
-                <span>{matchTime}</span>
+                <span suppressHydrationWarning={true}>{matchTime}</span>
             </div>
             <div className={styleOptions}>
                 <span>{matchEta}</span>
