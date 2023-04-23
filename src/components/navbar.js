666abb26fbd11dec3787bdbc55ed50af404c93fe
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-22 20:32:37 -0400

    use Image component for logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,16 @@
 import NavItem from "./navItem";
+import logo from '../../public/logo.svg'
+import Image from 'next/image'
 
 export default function NavBar({ matchView, onViewClick }) {
     return (
         <header className='pt-4 md:pt-0 flex flex-col md:flex-row m-6 items-center'>
-            <img className='logo my-2 md:my-0' src='logo.svg' alt='POSTPLANT' />
+            <Image className='my-2 md:my-0'
+                priority={true}
+                src={logo}
+                width={200}
+                height={34.6666666}
+                alt='POSTPLANT' />
             <nav className="w-full md:ml-6 my-2 md:my-0">
                 <ul className="grid grid-cols-2 md:flex">
                     <NavItem
