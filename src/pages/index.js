bdb0af9ec44294a3f198205f38993c076eebdafd
Erik Opinaldo <erikopinaldo@gmail.com>
2023-04-30 16:11:14 -0400

    import fontawesome stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import 'react-tooltip/dist/react-tooltip.css'
 import { Tooltip } from 'react-tooltip'
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
